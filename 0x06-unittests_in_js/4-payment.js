5af7979edc9417da9045d6bc4f11b7b8c7831d6d
El Houssain Souhail <sou.ductive@gmail.com>
2024-06-21 13:17:07 +0100

    Added task 4

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalSum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalSum}`);
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
 };
 
 module.exports = sendPaymentRequestToApi;
